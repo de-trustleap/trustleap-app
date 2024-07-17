7f5540a412e99bdc5d33bed75ab3ebcffe972f1b
Marcel Spindler <spindler.marcel@gmx.net>
2024-07-17 14:06:48 +0200

    added more company cubit tests

--- test/blocs/company_cubit_test.dart
+++ test/blocs/company_cubit_test.dart
@@ -2,9 +2,11 @@ import 'package:dartz/dartz.dart';
 import 'package:finanzbegleiter/application/profile/company/company_cubit.dart';
 import 'package:finanzbegleiter/core/failures/database_failures.dart';
 import 'package:finanzbegleiter/domain/entities/company.dart';
+import 'package:finanzbegleiter/domain/entities/company_request.dart';
 import 'package:finanzbegleiter/domain/entities/id.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+
 import '../repositories/auth_repository_test.mocks.dart';
 import '../repositories/company_repository_test.mocks.dart';
 
@@ -69,6 +71,149 @@ void main() {
       // Then
       expectLater(companyCubit.stream, emitsInOrder(expectedResult));
       companyCubit.updateCompany(testCompany);
+    });
+  });
+
+  group("CompanyCubit_getCompany", () {
+    const String companyID = "1";
+    final testCompany = Company(
+        id: UniqueID.fromUniqueString(companyID),
+        name: "Test",
+        industry: "Test");
+    test("should call company repo if function is called", () async {
+      // Given
+      when(mockCompanyRepo.getCompany(companyID))
+          .thenAnswer((_) async => right(testCompany));
+      // When
+      companyCubit.getCompany(companyID);
+      await untilCalled(mockCompanyRepo.getCompany(companyID));
+      // Then
+      // Then
+      verify(mockCompanyRepo.getCompany(companyID));
+      verifyNoMoreInteractions(mockCompanyRepo);
+    });
+
+    test(
+        "should emit GetCompanyLoadingState and then GetCompanySuccessState when function is called",
+        () {
+      // Given
+      final expectedResult = [
+        GetCompanyLoadingState(),
+        GetCompanySuccessState(company: testCompany)
+      ];
+      when(mockCompanyRepo.getCompany(companyID))
+          .thenAnswer((_) async => right(testCompany));
+      // Then
+      expectLater(companyCubit.stream, emitsInOrder(expectedResult));
+      companyCubit.getCompany(companyID);
+    });
+
+    test(
+        "should emit GetCompanyLoadingState and then GetCompanyFailureState when function is called and there was an error",
+        () {
+      // Given
+      final expectedResult = [
+        GetCompanyLoadingState(),
+        GetCompanyFailureState(failure: BackendFailure())
+      ];
+      when(mockCompanyRepo.getCompany(companyID))
+          .thenAnswer((_) async => left(BackendFailure()));
+      // Then
+      expectLater(companyCubit.stream, emitsInOrder(expectedResult));
+      companyCubit.getCompany(companyID);
+    });
+  });
+
+  group("CompanyCubit_registerCompany", () {
+    final testCompany = Company(
+        id: UniqueID.fromUniqueString("1"), name: "Test", industry: "Test");
+    test("should call company repo if function is called", () async {
+      // Given
+      when(mockCompanyRepo.registerCompany(testCompany))
+          .thenAnswer((_) async => right(unit));
+      // When
+      companyCubit.registerCompany(testCompany);
+      await untilCalled(mockCompanyRepo.registerCompany(testCompany));
+      // Then
+      verify(mockCompanyRepo.registerCompany(testCompany));
+      verifyNoMoreInteractions(mockCompanyRepo);
+    });
+
+    test(
+        "should emit CompanyRegisterLoadingState and then CompanyRegisterSuccessState when function is called",
+        () async {
+      // Given
+      final expectedResult = [
+        CompanyRegisterLoadingState(),
+        CompanyRegisterSuccessState()
+      ];
+      when(mockCompanyRepo.registerCompany(testCompany))
+          .thenAnswer((_) async => right(unit));
+      // Then
+      expectLater(companyCubit.stream, emitsInOrder(expectedResult));
+      companyCubit.registerCompany(testCompany);
+    });
+
+    test(
+        "should emit CompanyRegisterLoadingState and then CompanyRegisterFailureState when function is called and there was an error",
+        () async {
+      // Given
+      final expectedResult = [
+        CompanyRegisterLoadingState(),
+        CompanyRegisterFailureState(failure: BackendFailure())
+      ];
+      when(mockCompanyRepo.registerCompany(testCompany))
+          .thenAnswer((_) async => left(BackendFailure()));
+      // Then
+      expectLater(companyCubit.stream, emitsInOrder(expectedResult));
+      companyCubit.registerCompany(testCompany);
+    });
+  });
+
+  group("CompanyCubit_getPendingCompanyRequest", () {
+    const requestID = "1";
+    final testCompanyRequest =
+        CompanyRequest(id: UniqueID.fromUniqueString(requestID));
+    test("should call company repo if function is called", () async {
+      // Given
+      when(mockCompanyRepo.getPendingCompanyRequest(requestID))
+          .thenAnswer((_) async => right(testCompanyRequest));
+      // When
+      companyCubit.getPendingCompanyRequest(requestID);
+      await untilCalled(mockCompanyRepo.getPendingCompanyRequest(requestID));
+      // Then
+      verify(mockCompanyRepo.getPendingCompanyRequest(requestID));
+      verifyNoMoreInteractions(mockCompanyRepo);
+    });
+
+    test(
+        "should emit PendingCompanyRequestLoadingState and then PendingCompanyRequestSuccessState when function is called",
+        () async {
+      // Given
+      final expectedResult = [
+        PendingCompanyRequestLoadingState(),
+        PendingCompanyRequestSuccessState(request: testCompanyRequest)
+      ];
+      when(mockCompanyRepo.getPendingCompanyRequest(requestID))
+          .thenAnswer((_) async => right(testCompanyRequest));
+      // Then
+      expectLater(companyCubit.stream, emitsInOrder(expectedResult));
+      companyCubit.getPendingCompanyRequest(requestID);
+    });
+
+    test(
+        "should emit PendingCompanyRequestLoadingState and then PendingCompanyRequestFailureState when function is called and there was an error",
+        () async {
+      // Given
+      final expectedResult = [
+        PendingCompanyRequestLoadingState(),
+        PendingCompanyRequestFailureState(failure: BackendFailure())
+      ];
+      when(mockCompanyRepo.getPendingCompanyRequest(requestID))
+          .thenAnswer((_) async => left(BackendFailure()));
+      // Then
+      expectLater(companyCubit.stream, emitsInOrder(expectedResult));
+      companyCubit.getPendingCompanyRequest(requestID);
     });
   });
 }
