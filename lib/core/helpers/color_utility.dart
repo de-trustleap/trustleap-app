13844eaabf128c691498b54c296cb8fbc72cb991
Marcel Spindler <spindler.marcel@gmx.net>
2024-09-27 17:58:35 +0200

    removed print

--- lib/core/helpers/color_utility.dart
+++ lib/core/helpers/color_utility.dart
@@ -1,6 +1,5 @@
 class ColorUtility {
   static int getHexIntFromString(String hexCode) {
-    print("THE COLOR: $hexCode");
     final colorCode = int.tryParse(hexCode, radix: 16);
     if (colorCode == null) {
       return 0x00000000;
