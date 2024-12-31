b224d60244315161e30811d4b4463f1050239a1c
Marcel Spindler <spindler.marcel@gmx.net>
2024-12-31 14:23:37 +0100

    removed editable text and used normal text widget to show text in pagebuilder

---
+++ lib/presentation/page_builder/page_elements/pagebuilder_text.dart
@@ -0,0 +1,21 @@
+import 'package:finanzbegleiter/domain/entities/pagebuilder/pagebuilder_text_properties.dart';
+import 'package:finanzbegleiter/domain/entities/pagebuilder/pagebuilder_widget.dart';
+import 'package:finanzbegleiter/presentation/page_builder/page_elements/textstyle_parser.dart';
+import 'package:finanzbegleiter/presentation/page_builder/top_level_components/landing_page_builder_widget_container.dart';
+import 'package:flutter/material.dart';
+
+class PagebuilderText extends StatelessWidget {
+  final PageBuilderTextProperties properties;
+  final PageBuilderWidget widgetModel;
+  const PagebuilderText(
+      {super.key, required this.properties, required this.widgetModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return LandingPageBuilderWidgetContainer(
+        model: widgetModel,
+        child: Text(properties.text ?? "",
+            style: TextStyleParser().getTextStyleFromProperties(properties),
+            textAlign: properties.alignment));
+  }
+}
