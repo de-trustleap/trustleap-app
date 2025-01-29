ce5e9879c2011129a982f9681ed62b233164fc64
Marcel Spindler <spindler.marcel@gmx.net>
2025-01-29 14:00:34 +0100

    added tests

---
+++ test/models/models/pagebuilder/pagebuilder_footer_properties_model_test.dart
@@ -0,0 +1,234 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:finanzbegleiter/domain/entities/pagebuilder/pagebuilder_footer_properties.dart';
+import 'package:finanzbegleiter/domain/entities/pagebuilder/pagebuilder_text_properties.dart';
+import 'package:finanzbegleiter/infrastructure/models/pagebuilder/pagebuilder_footer_properties_model.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  group("PagebuilderFooterPropertiesModel_CopyWith", () {
+    test(
+        "set privacyPolicyTextProperties with copyWith should set privacyPolicyTextProperties for resulting object",
+        () {
+      // Given
+      final model = PagebuilderFooterPropertiesModel(
+          privacyPolicyTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          },
+          impressumTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          });
+      final expectedResult = PagebuilderFooterPropertiesModel(
+          privacyPolicyTextProperties: {
+            "text": "Test",
+            "fontSize": 16.0,
+            "fontFamily": "Poppins"
+          },
+          impressumTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          });
+
+      // When
+      final result = model.copyWith(privacyPolicyTextProperties: {
+        "text": "Test",
+        "fontSize": 16.0,
+        "fontFamily": "Poppins"
+      });
+      // Then
+      expect(result, expectedResult);
+    });
+  });
+
+  group("PagebuilderFooterPropertiesModel_ToMap", () {
+    test("check if model is successfully converted to a map", () {
+      // Given
+      final model = PagebuilderFooterPropertiesModel(
+          privacyPolicyTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          },
+          impressumTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          });
+      final expectedResult = {
+        "privacyPolicyTextProperties": {
+          "text": "Test",
+          "fontSize": 14.0,
+          "fontFamily": "Poppins"
+        },
+        "impressumTextProperties": {
+          "text": "Test",
+          "fontSize": 14.0,
+          "fontFamily": "Poppins"
+        }
+      };
+      // When
+      final result = model.toMap();
+      // Then
+      expect(result, expectedResult);
+    });
+  });
+
+  group("PagebuilderFooterPropertiesModel_FromMap", () {
+    test("check if map is successfully converted to model", () {
+      // Given
+      final map = {
+        "privacyPolicyTextProperties": {
+          "text": "Test",
+          "fontSize": 14.0,
+          "fontFamily": "Poppins"
+        },
+        "impressumTextProperties": {
+          "text": "Test",
+          "fontSize": 14.0,
+          "fontFamily": "Poppins"
+        }
+      };
+      final expectedResult = PagebuilderFooterPropertiesModel(
+          privacyPolicyTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          },
+          impressumTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          });
+      // When
+      final result = PagebuilderFooterPropertiesModel.fromMap(map);
+      // Then
+      expect(result, expectedResult);
+    });
+  });
+
+  group("PagebuilderFooterPropertiesModel_ToDomain", () {
+    test(
+        "check if conversion from PagebuilderFooterPropertiesModel to PagebuilderFooterProperties works",
+        () {
+      // Given
+      final model = PagebuilderFooterPropertiesModel(
+          privacyPolicyTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          },
+          impressumTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          });
+      final expectedResult = PagebuilderFooterProperties(
+          privacyPolicyTextProperties: PageBuilderTextProperties(
+              text: "Test",
+              fontSize: 14.0,
+              fontFamily: "Poppins",
+              lineHeight: null,
+              letterSpacing: null,
+              color: null,
+              alignment: TextAlign.left,
+              textShadow: null,
+              isBold: null,
+              isItalic: null),
+          impressumTextProperties: PageBuilderTextProperties(
+              text: "Test",
+              fontSize: 14.0,
+              fontFamily: "Poppins",
+              lineHeight: null,
+              letterSpacing: null,
+              color: null,
+              alignment: TextAlign.left,
+              textShadow: null,
+              isBold: null,
+              isItalic: null));
+      // When
+      final result = model.toDomain();
+      // Then
+      expect(result, expectedResult);
+    });
+  });
+
+  group("PagebuilderFooterPropertiesModel_FromDomain", () {
+    test(
+        "check if conversion from PagebuilderFooterProperties to PagebuilderFooterPropertiesModel works",
+        () {
+      // Given
+      final model = PagebuilderFooterProperties(
+          privacyPolicyTextProperties: PageBuilderTextProperties(
+              text: "Test",
+              fontSize: 14.0,
+              fontFamily: "Poppins",
+              lineHeight: null,
+              letterSpacing: null,
+              color: null,
+              alignment: null,
+              textShadow: null,
+              isBold: null,
+              isItalic: null),
+          impressumTextProperties: PageBuilderTextProperties(
+              text: "Test",
+              fontSize: 14.0,
+              fontFamily: "Poppins",
+              lineHeight: null,
+              letterSpacing: null,
+              color: null,
+              alignment: null,
+              textShadow: null,
+              isBold: null,
+              isItalic: null));
+      final expectedResult = PagebuilderFooterPropertiesModel(
+          privacyPolicyTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          },
+          impressumTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          });
+      // When
+      final result = PagebuilderFooterPropertiesModel.fromDomain(model);
+      // Then
+      expect(result, expectedResult);
+    });
+  });
+
+  group("PagebuilderFooterPropertiesModel_Props", () {
+    test("check if value equality works", () {
+      // Given
+      final properties1 = PagebuilderFooterPropertiesModel(
+          privacyPolicyTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          },
+          impressumTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          });
+      final properties2 = PagebuilderFooterPropertiesModel(
+          privacyPolicyTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          },
+          impressumTextProperties: {
+            "text": "Test",
+            "fontSize": 14.0,
+            "fontFamily": "Poppins"
+          });
+      // Then
+      expect(properties1, properties2);
+    });
+  });
+}
