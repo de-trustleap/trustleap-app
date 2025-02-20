0166d36deca071d0575e4ec51bcc7da7d3cd5a3f
Vladi <vzbuys@gmail.com>
2025-02-20 07:14:41 +0100

    UniqueID verwendet in leaditem

--- lib/domain/entities/leadItem.dart
+++ lib/domain/entities/leadItem.dart
@@ -1,16 +1,21 @@
 import 'package:equatable/equatable.dart';
+import 'package:finanzbegleiter/domain/entities/id.dart';
 
 class LeadItem extends Equatable {
+  final String id;
   final String name;
   final String reason;
   final String promotionTemplate;
 
-  const LeadItem({
-    required this.name
+  static final _uuid = UniqueID();
+
+  LeadItem({
+    String? id
+  , required this.name
   , required this.reason
   , required this.promotionTemplate
-  });
+  }): id = id ?? _uuid.toString();
 
   @override
-  List<Object?> get props => [name, reason];
+  List<Object?> get props => [id, name, reason];
 }
