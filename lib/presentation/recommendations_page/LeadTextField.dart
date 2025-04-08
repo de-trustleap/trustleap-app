92f8a672abc64bf725578ff3756342615cb2603a
Vladi <vzbuys@gmail.com>
2025-04-08 18:14:22 +0200

    primary_button um icon erweitert

--- lib/presentation/recommendations_page/LeadTextField.dart
+++ lib/presentation/recommendations_page/LeadTextField.dart
@@ -1,5 +1,6 @@
 import 'package:finanzbegleiter/l10n/generated/app_localizations.dart';
 import 'package:finanzbegleiter/presentation/core/shared_elements/widgets/form_textfield.dart';
+import 'package:finanzbegleiter/presentation/core/shared_elements/widgets/primary_button.dart';
 import 'package:flutter/material.dart';
 
 class LeadTextField extends StatelessWidget {
@@ -21,27 +22,19 @@ class LeadTextField extends StatelessWidget {
       children: [
         Expanded(
           child: FormTextfield(
-            controller: controller,
-            disabled: false,
-            placeholder: "${localization.recommendation_page_leadTextField_title_prefix} $leadName",
-            minLines: 10,
-            maxLines: 10,
-            keyboardType: TextInputType.multiline
-          ),
+              controller: controller,
+              disabled: false,
+              placeholder:
+                  "${localization.recommendation_page_leadTextField_title_prefix} $leadName",
+              minLines: 10,
+              maxLines: 10,
+              keyboardType: TextInputType.multiline),
         ),
         const SizedBox(height: 10),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(localization.recommendation_page_leadTextField_send_button),
-            const SizedBox(width: 8.0),
-            IconButton(
-              onPressed: onSendPressed,
-              icon: const Icon(Icons.send),
-              color: Theme.of(context).colorScheme.primary,
-            ),
-          ],
-        )
+        PrimaryButton(
+            title: localization.recommendation_page_leadTextField_send_button,
+            onTap: onSendPressed,
+            icon: Icons.send),
       ],
     );
   }
