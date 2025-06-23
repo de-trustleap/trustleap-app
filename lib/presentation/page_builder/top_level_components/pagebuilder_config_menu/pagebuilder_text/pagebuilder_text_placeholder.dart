5bd9cd5b2aa59f9efec782d232b820ca0f799241
Marcel <spindler.marcel@gmx.net>
2025-06-23 16:11:19 +0200

    fixed redirection issue (#158)

--- lib/presentation/page_builder/top_level_components/pagebuilder_config_menu/pagebuilder_text/pagebuilder_text_placeholder.dart
+++ lib/presentation/page_builder/top_level_components/pagebuilder_config_menu/pagebuilder_text/pagebuilder_text_placeholder.dart
@@ -1,8 +1,4 @@
 class PagebuilderTextPlaceholder {
-  static const promoterFirstName = "[promoterFirstName]";
-  static const promoterLastName = "[promoterLastName]";
   static const promoterName = "[promoterName]";
   static const recommendationName = "[recommendationName]";
 }
-
-// TODO: PromoterName muss in lastName und firstName getrennt werden in recommendations Collection.
