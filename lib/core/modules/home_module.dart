9109da4dfb66e270fe0ac568bc84cdbe24ec71e3
Marcel <spindler.marcel@gmx.net>
2025-06-24 10:47:00 +0200

    added link to landingpage preview (#160)

--- lib/core/modules/home_module.dart
+++ lib/core/modules/home_module.dart
@@ -3,7 +3,6 @@ import 'package:finanzbegleiter/presentation/core/page_wrapper/home_page.dart';
 import 'package:finanzbegleiter/presentation/dashboard_page/dashboard_page.dart';
 import 'package:finanzbegleiter/presentation/landing_page/landing_page.dart';
 import 'package:finanzbegleiter/presentation/landing_page/widgets/landing_page_creator/landing_page_creator.dart';
-import 'package:finanzbegleiter/presentation/landing_page/widgets/landing_page_site/landing_page_site.dart';
 import 'package:finanzbegleiter/presentation/page_builder/landing_page_builder_view.dart';
 import 'package:finanzbegleiter/presentation/profile_page/profile_page.dart';
 import 'package:finanzbegleiter/presentation/profile_page/widgets/company_registration/company_registration_page.dart';
@@ -53,10 +52,6 @@ class HomeModule extends Module {
           ChildRoute(RoutePaths.activitiesPath,
               child: (_) => const ActivityPage()),
         ]);
-    r.child(
-        "${Modular.initialRoute}${RoutePaths.landingPagePath}/:landingPageId",
-        child: (_) =>
-            LandingPageSite(landingPageId: r.args.params['landingPageId']));
     r.child("${Modular.initialRoute}${RoutePaths.landingPageBuilderPath}/:id",
         child: (_) => const LandingPageBuilderView());
     r.wildcard(child: (_) => const DashboardPage());
