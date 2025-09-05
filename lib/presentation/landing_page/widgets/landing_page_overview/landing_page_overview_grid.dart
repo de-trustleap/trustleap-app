05a0823f539c3836e03eac517bf63f00d6d5fab5
Marcel Spindler <spindler.marcel@gmx.net>
2025-09-05 16:18:19 +0200

    fixed landingpages aspect ratio issue

--- lib/presentation/landing_page/widgets/landing_page_overview/landing_page_overview_grid.dart
+++ lib/presentation/landing_page/widgets/landing_page_overview/landing_page_overview_grid.dart
@@ -28,7 +28,7 @@ class LandingPageOverviewGrid extends StatelessWidget {
 
   final maxLandingPageCount = 11;
   final double tileWidth = 200;
-  final double aspectRatio = 7;
+  final double aspectRatio = 0.7;
 
   @override
   Widget build(BuildContext context) {
@@ -124,3 +124,7 @@ class LandingPageOverviewGrid extends StatelessWidget {
     );
   }
 }
+
+// TODO: Empfehlungen fehlt SizedBox nach unten
+// TODO: Bei Tabbar schauen ob sie scrollbar sein muss
+// TODO: Side Menu soll sich schließen wenn man einen Menüpunkt gewählt hat
