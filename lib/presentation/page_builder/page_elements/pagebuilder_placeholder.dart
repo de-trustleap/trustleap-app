0baa8d772491b376f2e163c7cb61e9b10ddb84ff
Marcel <spindler.marcel@gmx.net>
2025-10-20 11:51:50 +0200

    you can now drag into placeholder and zoom functionality is now without spacing (#388)

--- lib/presentation/page_builder/page_elements/pagebuilder_placeholder.dart
+++ lib/presentation/page_builder/page_elements/pagebuilder_placeholder.dart
@@ -1,4 +1,5 @@
 import 'package:finanzbegleiter/application/pagebuilder/pagebuilder_bloc.dart';
+import 'package:finanzbegleiter/domain/entities/pagebuilder/drag_data/widget_library_drag_data.dart';
 import 'package:finanzbegleiter/domain/entities/pagebuilder/pagebuilder_widget.dart';
 import 'package:finanzbegleiter/presentation/page_builder/page_elements/pagebuilder_add_widget_overlay.dart';
 import 'package:finanzbegleiter/presentation/page_builder/page_elements/pagebuilder_overlay.dart';
@@ -21,6 +22,7 @@ class PagebuilderPlaceholder extends StatefulWidget {
 
 class _PagebuilderPlaceholderState extends State<PagebuilderPlaceholder> {
   bool _isHovered = false;
+  bool _isDragOver = false;
 
   void _showWidgetMenu(BuildContext context) {
     PagebuilderOverlay.show(
@@ -37,41 +39,67 @@ class _PagebuilderPlaceholderState extends State<PagebuilderPlaceholder> {
     );
   }
 
+  void _replacePlaceholder(WidgetLibraryDragData dragData) {
+    Modular.get<PagebuilderBloc>().add(ReplacePlaceholderEvent(
+      placeholderId: widget.widgetModel.id.value,
+      widgetType: dragData.widgetType,
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
 
-    return MouseRegion(
-      onEnter: (_) => setState(() => _isHovered = true),
-      onExit: (_) => setState(() => _isHovered = false),
-      cursor: SystemMouseCursors.click,
-      child: Builder(
-        builder: (context) {
-          return InkWell(
-            onTap: () => _showWidgetMenu(context),
-            child: Container(
-              height: 200,
-              decoration: BoxDecoration(
-                color: _isHovered
-                    ? themeData.colorScheme.secondary.withValues(alpha: 0.1)
-                    : Colors.transparent,
-                border: Border.all(
-                  color: themeData.colorScheme.secondary,
-                  width: 2,
-                  style: BorderStyle.solid,
+    return DragTarget<WidgetLibraryDragData>(
+      onWillAcceptWithDetails: (details) => true,
+      onAcceptWithDetails: (details) {
+        _replacePlaceholder(details.data);
+        setState(() => _isDragOver = false);
+      },
+      onLeave: (_) {
+        setState(() => _isDragOver = false);
+      },
+      onMove: (_) {
+        if (!_isDragOver) {
+          setState(() => _isDragOver = true);
+        }
+      },
+      builder: (context, candidateData, rejectedData) {
+        return MouseRegion(
+          onEnter: (_) => setState(() => _isHovered = true),
+          onExit: (_) => setState(() => _isHovered = false),
+          cursor: SystemMouseCursors.click,
+          child: Builder(
+            builder: (context) {
+              return InkWell(
+                onTap: () => _showWidgetMenu(context),
+                child: Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: _isDragOver
+                        ? themeData.colorScheme.secondary.withValues(alpha: 0.2)
+                        : _isHovered
+                            ? themeData.colorScheme.secondary.withValues(alpha: 0.1)
+                            : Colors.transparent,
+                    border: Border.all(
+                      color: themeData.colorScheme.secondary,
+                      width: _isDragOver ? 3 : 2,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.add,
+                      size: 48,
+                      color: themeData.colorScheme.secondary,
+                    ),
+                  ),
                 ),
-              ),
-              child: Center(
-                child: Icon(
-                  Icons.add,
-                  size: 48,
-                  color: themeData.colorScheme.secondary,
-                ),
-              ),
-            ),
-          );
-        },
-      ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
